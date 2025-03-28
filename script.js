c20937e9aa623e9335d9c94186d3335922d7d207
Masela Ogendo <puroselah@gmail.com>
2025-03-28 15:59:23 +0300

    initial .js set up,getting elements

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const recipesContainer = document.getElementById('recipes-container');
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
+    const categoryButtons = document.querySelectorAll('.category-btn');
+    const recipeDetails = document.getElementById('recipe-details');
+    const closeRecipeDetails = document.querySelector('.close-recipe-details');});
